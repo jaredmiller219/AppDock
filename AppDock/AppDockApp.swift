23e501737ef18e554e84f9aae5e0e8d3819f84fb
Jared Miller <jaredmiller219@gmail.com>
2024-12-12 22:47:06 -0500

    Initial Commit

---
+++ AppDock/AppDockApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppDockApp.swift
+//  AppDock
+//
+//  Created by Jared Miller on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct AppDockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppDockTests/AppDockTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppDockTests.swift
+//  AppDockTests
+//
+//  Created by Jared Miller on 12/12/24.
+//
+
+import Testing
+@testable import AppDock
+
+struct AppDockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
